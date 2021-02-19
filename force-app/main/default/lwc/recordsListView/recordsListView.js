d7f9ff8ae8e28668fdcfc4ad985c35727f4a66d3
mihirs16 <mihirs16@gmail.com>
2021-02-19 18:28:32 +0530

    added ticket controller

--- force-app/main/default/lwc/recordsListView/recordsListView.js
+++ force-app/main/default/lwc/recordsListView/recordsListView.js
@@ -1,11 +1,4 @@
-/**
- * custom component for displaying 
- * any given list of records as cards
- * ---------------------------------
- * desktop | mobile
- * ---------------------------------
- * Mihir Singh
- */
+
 import { api, LightningElement } from 'lwc';
 
 export default class RecordsListView extends LightningElement {
