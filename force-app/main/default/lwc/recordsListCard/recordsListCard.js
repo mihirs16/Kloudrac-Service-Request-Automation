89d34ece8ed645fb3d5ea5ec57b198434ee4fd71
mihirs16 <mihirs16@gmail.com>
2021-02-21 23:43:40 +0530

    optimized for mobile

--- force-app/main/default/lwc/recordsListCard/recordsListCard.js
+++ force-app/main/default/lwc/recordsListCard/recordsListCard.js
@@ -1,3 +1,11 @@
+/**
+* name: recordsListCard
+* author: Mihir Singh
+* create-date: 21-02-2021
+* for-desktop: yes
+* for-mobile: yes
+* Description: child component of recordsListView to display each row
+*/
 import { LightningElement, api } from 'lwc';
 
 export default class RecordsListCard extends LightningElement {
