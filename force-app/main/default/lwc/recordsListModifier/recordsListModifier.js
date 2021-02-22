fe4fff2a622b6ed36b6e680f7cd55f4d051684b0
mihirs16 <mihirs16@gmail.com>
2021-02-22 11:59:31 +0530

    added search UI for list

---
+++ force-app/main/default/lwc/recordsListModifier/recordsListModifier.js
@@ -0,0 +1,11 @@
+/**
+* name: RecordsListModifier
+* author: Mihir Singh
+* createDate: 22-02-2021
+* for-desktop: yes
+* for-mobile: yes
+* description: search and filter modifiers for recordsListView
+*/
+import { LightningElement } from 'lwc';
+
+export default class RecordsListModifier extends LightningElement {}
