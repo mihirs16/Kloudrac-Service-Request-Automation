c5028533f7bf6d1c16bdab21256ab373f0c98144
mihirs16 <mihirs16@gmail.com>
2021-02-22 16:57:08 +0530

    added header | refactored for nav

---
+++ force-app/main/default/lwc/supportDefaultHeader/supportDefaultHeader.js
@@ -0,0 +1,11 @@
+/**
+* name: supportDefaultHeader
+* author: Mihir Singh
+* createDate: 22-02-2021
+* for-desktop: yes
+* for-mobile: yes
+* description: default page header for the Support Page
+*/
+import { LightningElement } from 'lwc';
+
+export default class SupportDefaultHeader extends LightningElement {}
---
+++ force-app/main/default/lwc/supportPage/supportPage.js
@@ -0,0 +1,13 @@
+/**
+* name: SupportPage
+* author: Mihir Singh
+* createDate: 22-02-2021
+* for-desktop: yes
+* for-mobile: yes
+* description: default support page
+*/
+import { LightningElement, track } from 'lwc';
+
+export default class SupportPage extends LightningElement {
+    @track pageShowing = 'default';
+}
