27a5952ba9e60c9e7f6a7e68eb43e10171fd519d
mihirs16 <mihirs16@gmail.com>
2021-02-24 16:55:06 +0530

    added dynamic count viz component

---
+++ force-app/main/default/lwc/countViz/countViz.js
@@ -0,0 +1,32 @@
+/**
+* name: CountViz
+* author: Mihir Singh
+* createDate: 24-02-2021
+* for-desktop: yes
+* for-mobile: yes
+* description: lwc component for visualising object dat
+*/
+import { api, LightningElement, track } from 'lwc';
+import getCountToDisplay from '@salesforce/apex/CountVizController.getCountToDisplay';
+
+export default class CountViz extends LightningElement {
+    @api vizTitle = 'Tickets';
+    @api vizSubtitle = 'By Priority';
+    @api objName = 'Ticket__c';
+    @api filterBy = 'None';
+    @api filter = 'P3';
+    @track count = 0;
+
+    renderedCallback() {
+        getCountToDisplay({
+            objectName: this.objName,
+            filterBy: this.filterBy,
+            filter: this.filter
+        })
+        .then((res) => {
+            console.log(res);
+            this.count = res;
+        })
+        .catch(err => console.log(err));
+    }
+}
