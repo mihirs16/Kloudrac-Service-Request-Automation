3f822d2c6c70c3c7b7dbfab7c43ff185d183b821
mihirs16 <mihirs16@gmail.com>
2021-02-28 01:22:33 +0530

    updated page template | connected comments to apex

--- force-app/main/default/lwc/recentComments/recentComments.js
+++ force-app/main/default/lwc/recentComments/recentComments.js
@@ -6,8 +6,38 @@
 * for-mobile: yes
 * description: list view of recent comments for home page
 */
-import { LightningElement, track } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getRecentComments from '@salesforce/apex/TicketController.recentTicketCommentsForThisUser';
 
 export default class RecentComments extends LightningElement {
+    
     @track listOfRecentComments;
+    @api numOfRecentComments = 5;
+
+    constructor() {
+        super();
+        
+        getRecentComments({ numberOfRecentComments: this.numOfRecentComments ? this.numOfRecentComments : 5 })
+        .then((result) => {
+            for (var i = 0; i < result.length; i++) {
+                var Created_On__c = new Date(result[i].Opened_On__c);
+                result[i].Opened_On__c = Created_On__c.getDate().toString() + '-' + Created_On__c.getMonth().toString() + '-' + Created_On__c.getFullYear().toString(); 
+            }
+            this.listOfRecentComments = result;
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+    }
+
+    // utility: error toast
+    errorMessage(err) {
+        this.dispatchEvent (
+            new ShowToastEvent ({
+                title: err.body.pageErrors[0].statusCode.split('_').join(' '),
+                message: err.body.pageErrors[0].message,
+                variant: 'error'
+            })
+        );
+    }
 }
